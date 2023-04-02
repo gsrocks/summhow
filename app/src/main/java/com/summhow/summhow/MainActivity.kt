14849c41f4db5ca9a3811ed4772e9f8f1ad42d58
gsrocks <strigungr@gmail.com>
2023-04-02 20:40:26 +0300

    Create HomeScreen composable

--- app/src/main/java/com/summhow/summhow/MainActivity.kt
+++ app/src/main/java/com/summhow/summhow/MainActivity.kt
@@ -3,13 +3,7 @@ package com.summhow.summhow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.summhow.summhow.feature.home.HomeRoute
 import com.summhow.summhow.ui.theme.SummhowTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SummhowTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                HomeRoute()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SummhowTheme {
-        Greeting("Android")
     }
 }
